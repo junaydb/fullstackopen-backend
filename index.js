1f93c05a1454411746df3a5bd4a304d759792158
junayd-bhoyroo <hello@junaydbhoyroo.com>
2022-11-26 14:11:41 +0000

    update build

--- index.js
+++ index.js
@@ -45,43 +45,47 @@ app.get("/api/persons", (req, res) => {
   Person.find({}).then((result) => res.send(result));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  person ? res.json(person) : res.sendStatus(404);
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((result) => (result ? res.json(result) : res.sendStatus(404).end()))
+    .catch((err) => next(err));
 });
 
 app.post("/api/persons", (req, res) => {
   const newPerson = req.body;
-
-  if (!newPerson) {
-    return res.status(400).json({ error: " contact information missing " });
-  }
-
-  if (!newPerson.name || !newPerson.number) {
-    return res.status(400).json({ error: "must include name and number" });
-  }
-
   Person.create({ ...newPerson }).then(res.status(201).send(newPerson));
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const exists = Person.findById(req.params.id);
+  Person.findByIdAndRemove(req.params.id).then(res.sendStatus(204).end());
+});
 
-  if (exists) {
-    exists.remove().then(res.sendStatus(204));
-  } else {
-    res.status(400).json({
-      message: "This person appears to be deleted already. Refresh the page.",
-    });
-  }
+app.put("/api/persons/:id", (req, res, next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person)
+    .then(() => res.sendStatus(204).end())
+    .catch((err) => next(err));
 });
 
 const unknownEndpoint = (req, res) => {
   res.status(400).json({ error: "unknown endpoint " });
 };
 app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+  if (err.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id " });
+  }
+
+  console.log("error");
+
+  next(error);
+};
+app.use(errorHandler);
 
 // run server
 const PORT = process.env.PORT;
